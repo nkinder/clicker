61b7000c8c427ad2907ef4ea12665c64a5322006
Nathan Kinder <nkinder@redhat.com>
2012-08-09 20:23:32 -0700

    Add serial device button and status command support
    This adds support for status commands for rs232 devices.  One can
    define status commands and responses to be returned to client in
    the device config files.  Filtering and mapping of the actual
    response from the device into meaningful values can be performed.
    
    In addition, button presses for serial devices now works as well.
    One can specify a serial read timeout in the device config files,
    otherwise the clicker daemon defaults to a 1 second timeout.

--- lib/remote.py
+++ lib/remote.py
@@ -24,6 +24,7 @@ import serial
 import urllib2
 import ConfigParser
 import codecs
+import binascii
 
 # Fetch our logger
 logger = logging.getLogger('clickerLogger')
@@ -62,6 +63,19 @@ def device_press_button(device, button):
         rc = devices[device].press_button(button)
 
     return rc
+def device_list_status_cmds(device):
+    if device in devices:
+        return devices[device].list_status_cmds()
+    else:
+        return []
+
+def device_get_status(device, command):
+    retval = ''
+
+    if device in devices:
+        retval = devices[device].get_status(command)
+
+    return retval
 
 def activity_list():
     return sorted(activities.keys())
@@ -172,6 +186,22 @@ class Device:
         for button in self.buttons:
             self.buttons[button] = codecs.escape_decode(self.buttons[button])[0]
 
+        # Load the status commands and parsing info.
+        self.status_cmds = {}
+        if self.config.has_section('status_cmds'):
+            for cmd in self.config.items('status_cmds'):
+                try:
+                    self.status_cmds[cmd[0]] = StatusCommand(cmd[0], *(cmd[1].split(',')))
+                except Exception as e:
+                    logger.debug('Error loading status command ({0}):'.format(cmd[0]))
+                    logger.debug('{0}'.format(e))
+                    raise
+
+        # See if any response values are defined for any of the status commands.
+        for cmd in self.status_cmds.keys():
+            if self.config.has_section('{0}_values'.format(cmd)):
+                self.status_cmds[cmd].set_responses(self.config.items('{0}_values'.format(cmd)))
+
         # Parse communication mode options.
         if self.mode == 'rs232':
             self.mode = MODE_RS232
@@ -182,6 +212,7 @@ class Device:
             self.bytesize = serial.EIGHTBITS
             self.stopbits = serial.STOPBITS_ONE
             self.parity = serial.PARITY_NONE
+            self.timeout = 1
 
             # See if the config file overrides any defaults.
             if self.config.has_section('rs232'):
@@ -227,11 +258,16 @@ class Device:
                     else:
                         raise ConfigError('Illegal rs232 parity specified.  Legal values are none, even, odd.')
 
+                if self.config.has_option('rs232', 'timeout'):
+                    self.timeout = self.config.getint('rs232', 'timeout')
+
             # Open the connection to this device.
             try:
-                self.connection = serial.Serial(self.terminal, self.baud, self.bytesize, self.parity, self.stopbits, 1)
-            except serial.SerialException:
+                self.connection = serial.Serial(self.terminal, self.baud, self.bytesize, self.parity,
+                                                self.stopbits, self.timeout)
+            except serial.SerialException as e:
                 logger.warning('Unable to open rs232 connection to device {0}'.format(self.name))
+                logger.warning('{0}'.format(e))
                 self.connection = None
 
         elif self.mode == 'http':
@@ -262,34 +298,116 @@ class Device:
         else:
             raise ConfigError('Unknown mode.  Valid modes are rs232 and http.');
 
-    def status(self):
-        return 1
+    def reopen_connection(self):
+        if self.mode == MODE_RS232:
+            logger.debug('Reopening connection...')
+            try:
+                self.connection = serial.Serial(self.terminal, self.baud, self.bytesize, self.parity,
+                                                self.stopbits, self.timeout)
+            except serial.SerialException as e:
+                logger.warning('Unable to open rs232 connection to device {0}'.format(self.name))
+                logger.warning('{0}'.format(e))
+                self.connection = None
+                return 1
+
+            return 0
+        else:
+            # The http mode doesn't need the connection opened until we request a URL.
+            return 0
+
+    def list_status_cmds(self):
+        return sorted(self.status_cmds.keys())
+
+    def get_status(self, command):
+        logger.info('Received status request ({0}, {1})'.format(self.name, command))
+
+        if command in self.status_cmds:
+            # See what communication mode is used for this device and send the
+            # command accordingly.
+            if self.mode == MODE_RS232:
+                if not self.connection:
+                    if self.reopen_connection() != 0:
+                        return ''
+
+                logger.debug('Sending {0} command to device {1} ({2}, {3}, {4}, {5}, {6})'.
+                             format(codecs.escape_encode(self.status_cmds[command].command)[0], self.name,
+                                    self.terminal, self.baud, self.bytesize, self.stopbits, self.parity))
+
+                try:
+                    self.connection.write(self.status_cmds[command].command)
+                except serial.SerialException as e:
+                    logger.error('Unable to write to rs232 connection for device {0}'.format(self.name))
+                    logger.error('{0}'.format(e))
+                    return ''
+
+                try:
+                    response = self.connection.read(size=self.status_cmds[command].response_size)
+                except serial.SerialException as e:
+                    logger.error('Unable to read response from device {0}'.format(self.name))
+                    logger.error('{0}'.format(e))
+                    return ''
+
+                logger.debug('----- Received Response -----')
+                logger.debug('0x{0}'.format(binascii.hexlify(response)))
+
+                # Parse the result to find out what value we should return.
+                retval = self.status_cmds[command].get_result(response)
+                logger.debug('Returning value ({0})'.format(retval))
+
+                return retval
+            elif self.mode == MODE_HTTP:
+                logger.debug('Sending {0} to device {1}'.format(self.status_cmds[command], self.name))
+
+                try:
+                    f = urllib2.urlopen(url=self.status_cmds[command], timeout=self.timeout)
+
+                    # NGK TODO - responses from http are going to be very different from rs232.
+                    # Need to determine how we want to set up response config and deal with
+                    # parsing for http.
+                    logger.debug('----- Received Response -----')
+                    logger.debug(f.read())
+                except urllib2.URLError as e:
+                    # We could have sent the request, but just not received the response back
+                    # before the timeout.  We just log an error on the server side and return
+                    # an empty response to the client.
+                    logger.error('Error accessing URL:')
+                    logger.error(e.reason);
+
+                # NGK TODO - return actual response here after implementing http response handling.
+                return ''
+            else:
+                logger.warning('Unknown communication mode in get_status ({0}, {1})'.format(self.name, self.mode))
+                return ''
+        else:
+            logger.warning('Received request for non-existent status command ({0}, {1})'.format(self.name, command))
+            return ''
 
     def list_buttons(self):
         return sorted(self.buttons.keys())
 
     def press_button(self, button):
-        # Assume failure.
-        rc = 1
-
         logger.info('Received button press ({0}, {1})'.format(self.name, button))
 
         if button in self.buttons:
             # See what communication mode is used for this device and send the
             # command accordingly.
             if self.mode == MODE_RS232:
+                if not self.connection:
+                    if self.reopen_connection() != 0:
+                        return 1
+
                 logger.debug('Sending {0} command to device {1} ({2}, {3}, {4}, {5}, {6})'.
-                            format(codecs.escape_encode(self.buttons[button])[0], self.name,
-                                   self.terminal, self.baud, self.bytesize, self.stopbits, self.parity))
+                             format(codecs.escape_encode(self.buttons[button])[0], self.name,
+                                    self.terminal, self.baud, self.bytesize, self.stopbits, self.parity))
 
-                if self.connection:
-                    # NGK - send the command.
-                    logger.debug('Have connection')
-                else:
-                    # NGK - do we need to do this check and open the connection?
-                    logger.debug('No connection')
+                try:
+                    self.connection.write(self.buttons[button])
+                except serial.SerialException as e:
+                    logger.error('Unable to write to rs232 connection for device {0}'.format(self.name))
+                    logger.error('{0}'.format(e))
+                    return 1
 
-                rc = 0
+                return 0
             elif self.mode == MODE_HTTP:
                 logger.debug('Sending {0} to device {1}'.format(self.buttons[button], self.name))
 
@@ -307,15 +425,72 @@ class Device:
                     logger.error('Error accessing URL:')
                     logger.error(e.reason);
 
-                rc = 0
+                return 0
             else:
                 logger.warning('Unknown communication mode in press_button ({0}, {1})'.format(self.name, self.mode))
-                rc = 1
+                return 1
         else:
             logger.warning('Received request for non-existent button ({0}, {1})'.format(self.name, button))
-            rc = 1
+            return 1
 
-        return rc
+
+class StatusCommand:
+    """Represents a device status command and it's possible responses"""
+
+    def __init__(self, name, command, response_size, byte_filter, bit_filter, response_type):
+        self.name = name
+        # Decode any escaped hex chars that need to be sent to the device.
+        self.command = codecs.escape_decode(command)[0]
+        self.response_size = int(response_size)
+        self.byte_filter = byte_filter
+        self.bit_filter = bit_filter
+        self.response_type = response_type
+
+        # Initialize the response list
+        self.responses = []
+
+    def set_responses(self, responses):
+        self.responses = responses
+
+    def get_result(self, response):
+        # If the response is not the right size, just pretend the response was empty.
+        if len(response) != self.response_size:
+            logger.warning('Incorrect response size (got {0}, expected {1})'.format(len(response),
+                                                                                    self.response_size))
+            logger.warning('Ignoring response value.')
+            value = ''
+        else:
+            # Trim the bytes of interest.
+            trimmed = eval('response[' + self.byte_filter + ']')
+            logger.debug('----- Byte Trimmed Response -----')
+            logger.debug('0x{0}'.format(binascii.hexlify(trimmed)))
+
+            # Convert to requested type.  If the type isn't int, assume it's just a string.
+            if self.response_type == 'int':
+                # This takes our binary string data that was byte trimmed, hex encodes it,
+                # then converts it to an integer.  We need an integer so we can perform
+                # the bitwise operation specified in bit_filter.  After applying the
+                # bit filter, we then convert the result to a numeric string that can
+                # be returned to the client, or used in the regex below.
+                value = str(eval('int(binascii.hexlify(trimmed), 16)' + self.bit_filter))
+            else:
+                # The trimmed value is already a string.
+                value = trimmed
+
+        # If responses list is empty, we return the whole trimmed response (converted for type).
+        if len(self.responses) != 0:
+            # See which regex matches the trimmed response and return the appropriate value.
+            for resp in self.responses:
+                logger.debug('Comparing ({0}) to regex ({1})'.format(value, resp[1]))
+                if re.match(resp[1], value):
+                    logger.debug('Found a match.  Returning ({0})'.format(resp[0]))
+                    return resp[0]
+
+            # If we made it here, no regex matched.  Return an empty string.
+            logger.debug('No match found.')
+            return ''
+        else:
+            return value
 
 
 class Activity:
